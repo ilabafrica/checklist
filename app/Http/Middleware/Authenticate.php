52477d5f4d70a8d2d53dcb712ff00a2ebec4502b
Kitsao Emmanuel <kittemm@gmail.com>
2015-05-11 15:06:27 +0300

    redirect if not signed in

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -40,7 +40,7 @@ class Authenticate {
 			}
 			else
 			{
-				return redirect()->guest('auth/signin');
+				return redirect()->guest('auth/login');
 			}
 		}
 
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -5,6 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- Bootstrap -->
       <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+      <script src="{{ URL::asset('admin/js/jquery.min.js') }}"></script>
       <style type="text/css">
           .header{
               padding-top: 45px;
