9ce9dcba011fd835b13d1201b3f3da6e01a292e8
Kitsao Emmanuel <kittemm@gmail.com>
2015-05-18 20:32:32 +0300

    audit stars

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -35,7 +35,7 @@
         <div class="row">
         <div class="col-sm-12">
             <ul class="nav nav-tabs default">
-                <li class="active"><a href="#scores" data-toggle="tab" aria-expanded="true">{!! Lang::choice('messages.audit-score', 1) !!}</a>
+                <li class="active"><a href="#scores" data-toggle="tab" aria-expanded="true">{!! Lang::choice('messages.audited-score', 1) !!}</a>
                 </li>
                 <li class=""><a href="#bar" data-toggle="tab" aria-expanded="false">{!! Lang::choice('messages.bar-chart', 1) !!}</a>
                 </li>
@@ -46,37 +46,42 @@
                 <div class="tab-pane" id="spider" style="padding-top:20px;"></div>
                 <div class="tab-pane" id="bar" style="padding-top:20px;"></div>
                 <div class="tab-pane fade active in" id="scores" style="padding-top:20px;">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <table class="table table-striped table-bordered table-hover ">
-                                <thead>
-                                    <tr>{{ Lang::choice('messages.audit-score-sheet', 1) }}</tr>
-                                    <tr><th>{{ Lang::choice('messages.section', 1) }}</th>
-                                        <th>{{ Lang::choice('messages.label', 1) }}</th>
-                                        <th>{{ Lang::choice('messages.audited-score', 1) }}</th>
-                                        <th>{{ Lang::choice('messages.total-points', 1) }}</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($categories as $section)
-                                    <tr>
-                                        <td>{{ $section->name }}</td>
-                                        <td>{{ $section->label}}</td>
-                                        <td>{{ $section->subtotal($review->id)}}</td>
-                                        <td>{{ $section->total_points }}</td>
-                                    </tr>
-                                    @endforeach
-                                    <tr>
-                                        <strong>
-                                            <td colspan="2">{{ Lang::choice('messages.total-score', 1)  }}</td>
-                                            <td>{{ $score }}</td>
-                                            <td>{{ $points }}</td>
-                                        </strong>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                    <table class="table table-striped table-bordered table-hover ">
+                        <thead>
+                            <tr><th>{{ Lang::choice('messages.section', 1) }}</th>
+                                <th>{{ Lang::choice('messages.label', 1) }}</th>
+                                <th>{{ Lang::choice('messages.audited-score', 1) }}</th>
+                                <th>{{ Lang::choice('messages.total-points', 1) }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($categories as $section)
+                            <tr>
+                                <td>{{ $section->name }}</td>
+                                <td>{{ $section->label}}</td>
+                                <td>{{ $section->subtotal($review->id)}}</td>
+                                <td>{{ $section->total_points }}</td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="2"><strong>{{ Lang::choice('messages.total-score', 1)  }}</strong></td>
+                                <td><strong>{{ $score }}</strong></td>
+                                <td><strong>{{ $points }}</strong></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table table-striped table-bordered table-hover">
+                        <tbody>
+                            <tr>
+                                <td class="{!! round($score*100/$points, 2)<55?'warning':'' !!}"><h4>No Stars</h4><p>(0 - 142 pts)</p><p><i>&lt; 55%</i></p></td>
+                                <td class="{!! (round($score*100/$points, 2)>=55 && round($score*100/$points, 2)<65)?'warning':'' !!}"><h4>1 Star</h4><p>(143 - 165 pts)</p><p><i>55 - 64%</i></p></td>
+                                <td class="{!! (round($score*100/$points, 2)>=65 && round($score*100/$points, 2)<75)?'warning':'' !!}"><h4>2 Stars</h4><p>(166 - 191 pts)</p><p><i>65 - 74%</i></p></td>
+                                <td class="{!! (round($score*100/$points, 2)>=75 && round($score*100/$points, 2)<85)?'warning':'' !!}"><h4>3 Stars</h4><p>(192 - 217 pts)</p><p><i>75 - 84%</i></p></td>
+                                <td class="{!! (round($score*100/$points, 2)>=85 && round($score*100/$points, 2)<95)?'warning':'' !!}"><h4>4 Stars</h4><p>(218 - 243 pts)</p><p><i>85 - 94%</i></p></td>
+                                <td class="{!! round($score*100/$points, 2)>=95?'warning':'' !!}"><h4>5 Stars</h4><p>(244 - 258 pts)</p><p><i>&ge; 95%</i></p></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
